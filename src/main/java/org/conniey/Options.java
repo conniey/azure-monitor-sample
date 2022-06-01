c337bb1ea108928fbf1c7eacc0ae8e303e1f7bdd
Connie <conniey@microsoft.com>
2022-06-01 11:33:04 -0700

    Add options.

--- src/main/java/org/conniey/Options.java
+++ src/main/java/org/conniey/Options.java
@@ -3,17 +3,29 @@ package org.conniey;
 import com.beust.jcommander.IStringConverter;
 import com.beust.jcommander.Parameter;
 
+import java.time.Instant;
 import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Command line options
  */
 public class Options {
-    @Parameter(names = {"--start", "-s"}, description = "Start date and time for query", required = true,
+    @Parameter(names = {"--endpoint", "-e"}, description = "Endpoint for the workspace.", required = true)
+    private String endpoint;
+
+    @Parameter(names = {"--workspace", "-w"}, description = "Workspace Id.", required = true)
+    private String workspaceId;
+
+    @Parameter(names ={"--query", "-q"}, description = "Query to pass into Azure Monitor.", required = true)
+    private String query;
+
+    @Parameter(names = {"--start", "-s"}, description = "Start date and time in UTC for query (ie. 2011-12-03T10:15:30Z). If not set, last 24 hours is used.",
             converter = StringToDateConverter.class)
     private OffsetDateTime startDate;
 
-    @Parameter(names = {"--end", "-e"}, description = "End date and time for query", required = true,
+    @Parameter(names = {"--end", "-e"}, description = "End date and time in UTC for query (ie. 2011-12-03T10:15:30Z).  If not set, last 24 hours is used.",
             converter = StringToDateConverter.class)
     private OffsetDateTime endDate;
 
@@ -33,13 +45,39 @@ public class Options {
         this.startDate = startDate;
     }
 
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public String getWorkspaceId() {
+        return workspaceId;
+    }
+
+    public void setWorkspaceId(String workspaceId) {
+        this.workspaceId = workspaceId;
+    }
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
     /**
-     * Converts a string to its OffsetDateTime representation.
+     * Converts a string to its Instant representation.
      */
     static class StringToDateConverter implements IStringConverter<OffsetDateTime> {
         @Override
         public OffsetDateTime convert(String s) {
-            return OffsetDateTime.parse(s);
+            final Instant instant = DateTimeFormatter.ISO_INSTANT.parse(s, Instant::from);
+
+            return instant.atOffset(ZoneOffset.UTC);
         }
     }
 }
